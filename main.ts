fe93a8519196887cde4f0754e0b5407af9690e69
Bhunesh ojha108 <149822963+Bhuneshojha@users.noreply.github.com>
2024-03-25 20:06:00 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,34 @@
+//          100 days of coding challenge ( DAY 11 )
+
+//   Question 01    If-else in number varriable
+   let Users:string[] = []
+   if (Users.length === 0){
+      console.log("We Need to add more users");
+   }else {
+      console.log("great users!");
+   }
+//   Question 02
+let current_users: string[] = ["user1", "admin", "user3", "user4", "user5"];
+let new_users: string[] = ["User1", "User6", "user7", "admin", "User9"];
+
+new_users.forEach(newUser => {
+ if (current_users.some(currentUser => currentUser.toLowerCase() === newUser.toLowerCase())){
+    console.log(`${newUser} will need to enter a new username.`);
+  } else {
+        console.log(`${newUser} is available.`);
+    }
+});
+//   Question 03
+let numbers:number[]=[1,2,3,4,5,6,7,8,9];
+numbers.forEach(number =>{
+  let suffix = "th";
+  if (number === 1){
+    suffix = "st"
+  }else if ( number ===2){
+    suffix = "nd"
+  }else if ( number === 3){
+    suffix = "rd"
+  } 
+    console.log(`${number}${suffix}`);
+  
+});
